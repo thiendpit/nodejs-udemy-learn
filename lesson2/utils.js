541f45dfe9b32ca7f85fdb4f5f21ed47816b04e5
Kiều Phước Thiện <thien.dpit@gmail.com>
2020-02-01 15:41:53 +0700

    lesson2

---
+++ lesson2/utils.js
@@ -0,0 +1,12 @@
+console.log('utils.js');
+// const fi = 'Kieu Phuoc Thien';
+// const ok = 'okok';
+
+// module.exports = ok;
+// module.exports = fi;
+
+let sum = (a, b) => {
+    return a + b;
+}
+
+module.exports = sum;
